498044c013bf17cef7a70f4feb899d0e3223a7e1
Vaidik Kamde <vaidik16206138@gmail.com>
2021-03-25 01:05:49 +0530

    Added get request for the server

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios').default;
+const app = express();
+const PORT = 8080;
+app.use(cors());
+
+// async function getData(roll){
+//     let response = await axios.get(`https://terriblytinytales.com/testapi?rollnumber=${roll}`)
+//     console.log(response.data)
+//     return response.data;
+// }
+
+app.get('/api/getResult',async (req,res)=>{
+    const rollNumber = req.query.rollNumber.split(',')
+    let links =[]
+    rollNumber.forEach(roll => {
+        links.push(`https://terriblytinytales.com/testapi?rollnumber=${roll}`)
+    });
+    console.log(links);
+
+    let result = await axios.all(links.map(l=>axios.get(l))).then(axios.spread(function(...res){
+        console.log(res)
+        // return res.data
+        let result = res.map((r,index)=>{
+            if(r!==undefined){
+                return {rollNumber:rollNumber[index],result:r.data}
+            }else{
+                return undefined
+            }
+        })
+        console.log(result)
+        return result;
+    }))
+    console.log(result)
+    if (result !=undefined){
+        res.json(result);
+    }else{
+        res.sendStatus(404)
+    }
+
+});
+
+app.listen(process.env.PORT||PORT,()=>{
+    console.log(`Server Listning to port ${process.env.PORT||PORT}`);
+})
